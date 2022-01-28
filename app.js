62bc2249392cad619cf7f3ff2883e31460c3257a
BCila <b.cila51@gmail.com>
2022-01-28 12:59:49 +0300

    Lesson 02

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require('express');
-
+const ejs = require('ejs')
 const app = express();
 
+app.set("view engine", "ejs")
+
+// Middleware
+app.use(express.static("public"))
+
+// Routes
 app.get('/', (req, res) => {
-    res.send('index');
+    res.status(200).render('index',{
+        page_name: "index"
+    });
+});
+app.get('/about', (req, res) => {
+    res.status(200).render('about',{
+        page_name: "about"
+    });
 });
 
 
